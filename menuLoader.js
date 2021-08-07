c9161dda5cb34a1d7d05184fef3132ff625dd7ec
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2021-08-07 13:42:24 +0900

    menu test 2

--- menuLoader.js
+++ menuLoader.js
@@ -1,3 +1,8 @@
-$(function () {
-    $("div.top-bar").load("b.html");
-});
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "menu.html", true);
+xhr.onreadystatechange = function () {
+    if (this.readyState !== 4) return;
+    if (this.status !== 200) return; // or whatever error handling you want
+    document.getElementById("top-bar").innerHTML = this.responseText;
+};
+xhr.send();

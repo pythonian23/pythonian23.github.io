0783b138b32f87a76e22878bb41ab6e366ec457d
pythonaian23 <pythonaian23@gmail.com>
2022-05-23 00:14:15 +0900

    Visual changes

--- js/menuLoader.js
+++ js/menuLoader.js
@@ -1,8 +1,8 @@
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "menu.html", true);
 xhr.onreadystatechange = function () {
-    if (this.readyState !== 4) return;
-    if (this.status !== 200) return; // or whatever error handling you want
-    document.getElementById("top-bar").innerHTML = this.responseText;
+	if (this.readyState !== 4) return;
+	if (this.status !== 200) return; // or whatever error handling you want
+	document.getElementById("top-bar").innerHTML = this.responseText;
 };
 xhr.send();
